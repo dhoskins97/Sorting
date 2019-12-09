c303d2101a7df879714097421b5c007ef4b0e7be
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-12-09 17:40:39 -0500

    added comments to bubble_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -28,22 +28,32 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    
     # initialize a dynamic starting point at the beginning of the array
     needsToRun = True
 
-    # keep track of how many switches are made, and run this code until a switch is NOT made
+    # this while loop will run the pair comparisons through the whole list recursively until we flip the needsToRun value (aka no switches were made)
     while (needsToRun):
+        
+        # start the comparisons at the beginning of the array, and reset the value of the switchCounter
         DSP = 0
         switchCounter = 0
+
+        # this code checks single pair comparisons recursively until the last pair is checked
         while (DSP <= len(arr) - 2):
+            
             # check arr[DSP] and arr[DSP + 1] and if they are out of order, switch them and increment switch counter
             if arr[DSP] > arr[DSP + 1]:
                 arr[DSP], arr[DSP + 1] = arr[DSP + 1], arr[DSP]
                 switchCounter += 1
+            
             # increment our starting point
             DSP += 1
+        
+        # this runs after the whole list has been checked, and if no switches were made, we stop running the recursive checks and return the sorted array
         if switchCounter < 1:
             needsToRun = False
+            
     return arr
 
 
